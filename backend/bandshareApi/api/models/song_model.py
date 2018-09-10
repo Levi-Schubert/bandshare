982bb676cf003f0bf06f58cd97d3280972ca822d
Levi Schubert <me@levischubert.com>
2018-09-10 10:38:24 -0500

    added blank=true to song description

--- backend/bandshareApi/api/models/song_model.py
+++ backend/bandshareApi/api/models/song_model.py
@@ -5,7 +5,7 @@ class Song(models.Model):
 	band = models.ForeignKey('Profile', on_delete=models.CASCADE,)
 	title = models.CharField(max_length=128)
 	album = models.CharField(max_length=128)
-	description = models.CharField(max_length=256)
+	description = models.CharField(max_length=256, blank=True)
 	mp3 = models.FileField(upload_to='mp3/')
 
 	
